fa912ffaf35a1396fd7e35b651d81166854527cd
bugra <sadeceabk@gmail.com>
2023-01-31 00:16:36 +0300

    footter fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '../assets/images/koinbulteni_logo_1.png'
 const Footer = () => {
   return (
-    <div className='grid grid-cols-3 bg-h text-white py-8 px-2 sm:px-2 md:px-3 lg:px-9 xl:px-20'>
+    <div className='grid grid-cols-3 bg-h text-white py-8 mt-12 px-2 sm:px-2 md:px-3 lg:px-9 xl:px-20'>
       <div className='space-y-4'>
         <div>
           <span className='font-extrabold'>Koin Bülteni Basın Yayın Bilişim Organizasyon Anonim Şirketi</span>
